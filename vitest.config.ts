a5ae1d97f749776b5a2eafbe6e1c9d76f8748cac
souzajeinner-crypto <souzapereirajeinner@gmail.com>
2026-05-22 14:26:12 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,10 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as any],
   test: {
-    environment: "jsdom",
-    setupFiles: ["./tests/setup.ts"],
-    globals: true,
-    css: false,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
-});
+})
